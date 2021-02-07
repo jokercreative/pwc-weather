52ac61ec23a97de1ff17a81cbdc3376f22af53e8
Jodie Walker <jodie.walker@jokercreative.com>
2021-02-08 10:39:03 +1100

    adding weekly forcast with daily component

--- src/components/Forcast/Daily.js
+++ src/components/Forcast/Daily.js
@@ -1,4 +1,16 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
+import { format } from 'date-fns'
+import fromUnixTime from 'date-fns/fromUnixTime'
+import styled from 'styled-components'
+
+const Forcast = styled.div `
+  display: flex;
+  flex-direction: column;
+
+  @media (max-width: 768px) {
+    flex-direction: row;
+  }
+`
 
 class Daily extends Component {
 
@@ -6,14 +18,28 @@ class Daily extends Component {
     super(props);
 
     this.state = {
+
     }
   }
 
+  iconClass(desc) {
+    return `icon icon--${desc.toLowerCase()}`
+  }
+
   render() {
+
+    const {
+      forcast
+    } = this.props
+
     return (
-      <Fragment>
-        Daily
-      </Fragment>
+      <Forcast>
+        <span>{format(fromUnixTime(forcast.dt), 'EEEE')}</span>
+        <span>{forcast.weather[0].main}</span>
+        <i className={this.iconClass(forcast.weather[0].main)}></i>
+        <span>{forcast.temp.min}</span>
+        <span>{forcast.temp.max}</span>
+      </Forcast>
     )
   }
 }
--- src/components/Forcast/Weekly.js
+++ src/components/Forcast/Weekly.js
@@ -1,5 +1,17 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
+import styled from 'styled-components'
 import DailyForcast from './Daily'
+
+import {RepositoryFactory} from 'Repositories/RepositoryFactory';
+const WeatherRepository = RepositoryFactory.get('weather');
+
+const Forcast = styled.div `
+  display: flex;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`
 
 class Weekly extends Component {
 
@@ -7,22 +19,36 @@ class Weekly extends Component {
     super(props);
 
     this.state = {
+      forcast: []
+    }
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+    if (prevProps.coordinates !== this.props.coordinates) {
+      WeatherRepository.getForcast(this.props.coordinates)
+        .then((response) => {
+          this.setState({
+            forcast: response.daily
+          })
+        })
     }
   }
 
   render() {
 
     const {
-      mockdata
+      forcast
     } = this.state
 
     return (
-      <Fragment>
-        <DailyForcast />
-        {/* {mockdata.map(function(name, index){
-          return <DailyForcast />;
-        })} */}
-      </Fragment>
+      <Forcast>
+        {forcast.map(function(data, index){
+          return <DailyForcast
+            forcast={data}
+            key={index}
+          />
+        })}
+      </Forcast>
     )
   }
 }
