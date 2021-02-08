2fcc8e4ebce162094a338ec706b9cffabd9b4312
Jodie Walker <jodie.walker@jokercreative.com>
2021-02-08 16:06:39 +1100

    adding enzyme adapter for react 17

--- enzyme.config.js
+++ enzyme.config.js
@@ -1,3 +1,4 @@
 /** Used in jest.config.js */
-import { configure } from 'enzyme';
-configure({ adapter: new Adapter() });
+import { configure } from 'enzyme'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+configure({ adapter: new Adapter() })
